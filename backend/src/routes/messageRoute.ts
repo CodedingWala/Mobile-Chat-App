a645372d813ce9d62de21ca719af8e014c342661
Javed Moinuddin Ansari <ansarijaved092004@gmail.com>
2026-06-11 20:03:14 +0530

    routes files created succesfully

--- backend/src/routes/messageRoute.ts
+++ backend/src/routes/messageRoute.ts
@@ -1,3 +1,9 @@
-import{Router} from "express"
-const messageRoute=Router()
-export default messageRoute
+import { Router } from "express";
+import { protectRoute } from "../middleware/auth";
+import { getMessages } from "../controller/messageController";
+
+const router = Router();
+
+router.get("/chat/:chatId", protectRoute, getMessages);
+
+export default router;
