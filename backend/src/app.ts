2137766ac414cbb004056eddca8dc186168f861e
Javed Moinuddin Ansari <ansarijaved092004@gmail.com>
2026-06-11 18:55:03 +0530

    authentication route completed

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,14 +3,19 @@ import AuthRoute from "./routes/AuthRoutes";
 import messageRoute from "./routes/messageRoute";
 import chatRoute from "./routes/chatRoute";
 import userRoute from "./routes/userRoutes";
+import { clerkMiddleware } from '@clerk/express'
+import { errorHandler } from "./middleware/errorHandler";
 const app = express();
 
-
+app.use(express.json())
+app.use(clerkMiddleware())
 app.get("/health",(req,res)=>{
     res.json({message:"ok"})
 })
-app.use("/authRoute",AuthRoute)
-app.use("/messageRoute",messageRoute)
-app.use("/chatRoute",chatRoute)
-app.use("/userRoute",userRoute)
+app.use("/api/auth",AuthRoute)
+app.use("/api/message",messageRoute)
+app.use("/api/chat",chatRoute)
+app.use("/api/user",userRoute)
+
+app.use(errorHandler)
 export default app;
--- backend/src/routes/AuthRoutes.ts
+++ backend/src/routes/AuthRoutes.ts
@@ -1,3 +1,10 @@
-import{Router} from "express"
-const AuthRoute=Router()
-export default AuthRoute
+import { Router } from "express";
+import { protectRoute } from "../middleware/auth";
+import { authCallback, getMe } from "../controller/authController";
+
+const router = Router();
+
+router.get("/me", protectRoute, getMe);
+router.post("/callback", authCallback);
+
+export default router;
